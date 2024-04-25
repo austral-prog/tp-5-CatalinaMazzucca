d26e056b59d3d862a660a8540ad64f638e2eddcf
CatalinaMazzucca <162723477+CatalinaMazzucca@users.noreply.github.com>
2024-04-24 22:36:30 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -6,3 +6,14 @@ for cont in range(cant_numeros):
     if num > max_numero:
         max_numero = num
     print(num)
+
+cant_numeros = 1
+max_numero = 0
+for cont in range(cant_numeros):
+    num = int(input("Ingrese el primer numero: "))
+    num = int(input("Ingrese el segundo numero: "))
+    num = int(input("Ingrese el tercer numero: ")) 
+    if num > max_numero:
+        max_numero = num
+    print(num)
+
